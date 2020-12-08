eb7062f64c785fa62bd3292e4f45cb501717f6a3
sclin0323 <sclin0323@gmail.com>
2020-12-08 19:49:41 +0800

    Add new router Role

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,79 @@
 <template>
+  <a-layout id="components-layout-demo-custom-trigger">
+    <a-layout-sider v-model="collapsed" :trigger="null" collapsible>
+      <div class="logo" />
+      <a-menu theme="dark" mode="inline" :default-selected-keys="['1']">
+        <a-menu-item key="1">
+          <a-icon type="user" />
+          <span>HOME</span>
+          <router-link to="/"></router-link>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="video-camera" />
+          <span>ABOUT</span>
+          <router-link to="/about"></router-link>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <a-icon type="upload" />
+          <span>ROLE 管理</span>
+          <router-link to="/role"></router-link>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <a-icon
+          class="trigger"
+          :type="collapsed ? 'menu-unfold' : 'menu-fold'"
+          @click="() => (collapsed = !collapsed)"
+        />
+      </a-layout-header>
+      <a-layout-content
+        :style="{
+          margin: '24px 16px',
+          padding: '24px',
+          background: '#fff',
+          minHeight: '1024px'
+        }"
+      >
+        <router-view/>
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
+<style>
+#components-layout-demo-custom-trigger .trigger {
+  font-size: 18px;
+  line-height: 64px;
+  padding: 0 24px;
+  cursor: pointer;
+  transition: color 0.3s;
+}
+
+#components-layout-demo-custom-trigger .trigger:hover {
+  color: #1890ff;
+}
+
+#components-layout-demo-custom-trigger .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
+}
+</style>
+
+
+<!--
+<template>
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
@@ -30,3 +105,4 @@
   color: #42b983;
 }
 </style>
+-->
